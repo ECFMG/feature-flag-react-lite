a059f18b0f63e6c743d54be2e0c33b1b8cac1f72
Patrick Gidich <patrick@Patricks-MacBook-Pro.local>
2020-11-10 10:46:46 -0500

    initial commit

--- src/feature-flag-provider.tsx
+++ src/feature-flag-provider.tsx
@@ -9,7 +9,7 @@ import FeatureFlagContext, { FeatureFlags } from './feature-flag-context'
  */
 export interface FeatureFlagConfig {
   /** maximum age for the cache (in milliseconds), defaults to 30 seconds */
-  cache?: number 
+  cache?: number
   /** url to retrieve the feature flags from */
   url: string
   /** if url cannot be reached, load local feature flags */
@@ -26,7 +26,7 @@ export type FeatureFlagProps = {
 const FeatureFlagProvider: FC<FeatureFlagProps> = ( props: FeatureFlagProps ): JSX.Element => {
   const [featureFlagList, setFeatureFlagList] = useState<FeatureFlags | undefined>(props.config.fallbackFlagValues)
 
-  let featFlagList = featureFlagList??{}; //State is not coming into async function
+  let featFlagList = featureFlagList ?? {} //State is not coming into async function
   const cacheTimeout = !props.config.cache ? 30 * 1000 : props.config.cache
   const isRendered = React.useRef(false) // Used to make Async code not get called on every render.
 
@@ -58,33 +58,37 @@ const FeatureFlagProvider: FC<FeatureFlagProps> = ( props: FeatureFlagProps ): J
   })
 
   const cache = setupCache({
-    maxAge: cacheTimeout
+    maxAge: cacheTimeout,
+    readOnError: (error: any) => {
+      return error.response.status >= 400 && error.response.status < 600
+    },
+    clearOnStale: false
   })
 
   axiosRetry(axios, { retries: 3 })
-
   const remoteFlags = axios.create({
     adapter: cache.adapter
   })
- 
-  const GetFeatureFlags = async () => {
+
+  GetFeatureFlags = async () => {
     const options = {
-      url: props.config.url
+      url: props.config.url,
+      method: 'get'
+    } as AxiosRequestConfig
+    try {
+      var result = await remoteFlags(options)
+      if (JSON.stringify(featFlagList) !== JSON.stringify(result.data)) {
+        setFeatureFlagList(result.data)
+        featFlagList = result.data
+      }
+      console.log(`Reading from cache : ${result.request?.fromCache}`)
+      console.log(`Reading stale data due to network issue : ${result.request?.stale}`)
+
+      return featFlagList as FeatureFlags
+    } catch (ex: any) {
+      console.error('Fallback to local feature flags', ex)
+      setFeatureFlagList(props.config.fallbackFlagValues)
     }
-    var result = await remoteFlags(options)
-      .then((res) => {
-        //Compare featFlagList because state coming in is always undefined for some reason
-        if (JSON.stringify(featFlagList) !== JSON.stringify(res.data)) {
-          setFeatureFlagList(res.data);
-          featFlagList = res.data;
-        }
-        return featFlagList as FeatureFlags
-      })
-      .catch((ex: any) => {
-        console.error('Fallback to local feature flags', ex)
-        setFeatureFlagList(props.config.fallbackFlagValues)
-      })
-    return result
   }
 
   const getFeatureFlagByName = (name: string) => {
