fd3db8c863f6bee473072e3bb975c2221c5766ff
Patrick Gidich <patrick@Patricks-MacBook-Pro.local>
2020-11-10 16:05:12 -0500

    moving code into useEffect

--- src/feature-flag-provider.tsx
+++ src/feature-flag-provider.tsx
@@ -25,72 +25,66 @@ export type FeatureFlagProps = {
 
 const FeatureFlagProvider: FC<FeatureFlagProps> = ( props: FeatureFlagProps ): JSX.Element => {
   const [featureFlagList, setFeatureFlagList] = useState<FeatureFlags | undefined>(props.config.fallbackFlagValues)
-
-  let featFlagList = !featureFlagList ? {} : featureFlagList //State is not coming into async function
   const cacheTimeout = !props.config.cache ? 30 * 1000 : props.config.cache
   const isRendered = React.useRef(false) // Used to make Async code not get called on every render.
 
   useEffect(() => {
+    const setIntervalImmediately = (func: any, interval: number) => {
+      func()
+      return setInterval(func, interval)
+    }
+
+    axios.interceptors.request.use(async (axiosConfig) => {
+      if (typeof props.config.axiosRequestConfig === 'undefined') {
+        return axiosConfig
+      }
+      return await props.config.axiosRequestConfig(axiosConfig)
+    })
+
+    const cache = setupCache({
+      maxAge: cacheTimeout,
+      readOnError: (error: any) => {
+        return error.response.status >= 400 && error.response.status < 600
+      },
+      clearOnStale: false
+    })
+
+    axiosRetry(axios, { retries: 3 })
+    const remoteFlags = axios.create({
+      adapter: cache.adapter
+    })
+
+    const GetFeatureFlags = async () => {
+      const options = {
+        url: props.config.url,
+        method: 'get'
+      } as AxiosRequestConfig
+      try {
+        var result = await remoteFlags(options)
+        if (JSON.stringify(featureFlagList) !== JSON.stringify(result.data)) {
+          setFeatureFlagList(result.data)
+        }
+        console.log(`Reading from cache : ${result.request?.fromCache}`)
+        console.log(`Reading stale data due to network issue woopie: ${result.request?.stale}`)
+      } catch (ex) {
+        console.error('Fallback to local feature flags', ex)
+        setFeatureFlagList(props.config.fallbackFlagValues)
+      }
+    }
+
     ;(async () => {
       // IIFE to make async code work in a non-async Functional Component
       if (!isRendered.current) {
         setIntervalImmediately(
           async () => await GetFeatureFlags(),
-          cacheTimeout
+          cacheTimeout / 2
         )
       }
     })()
     return () => {
       isRendered.current = true
     }
-  }, [])
-
-  const setIntervalImmediately = (func: any, interval: number) => {
-    func()
-    return setInterval(func, interval)
-  }
-
-  axios.interceptors.request.use(async (axiosConfig) => {
-    if (typeof props.config.axiosRequestConfig === 'undefined') {
-      return axiosConfig
-    }
-    return await props.config.axiosRequestConfig(axiosConfig)
-  })
-
-  const cache = setupCache({
-    maxAge: cacheTimeout,
-    readOnError: (error: any) => {
-      return error.response.status >= 400 && error.response.status < 600
-    },
-    clearOnStale: false
-  })
-
-  axiosRetry(axios, { retries: 3 })
-  const remoteFlags = axios.create({
-    adapter: cache.adapter
-  })
-
-  const GetFeatureFlags = async () => {
-    const options = {
-      url: props.config.url,
-      method: 'get'
-    } as AxiosRequestConfig
-    try {
-      var result = await remoteFlags(options)
-      if (JSON.stringify(featFlagList) !== JSON.stringify(result.data)) {
-        setFeatureFlagList(result.data)
-        featFlagList = result.data
-      }
-      console.log(`Reading from cache : ${result.request?.fromCache}`)
-      console.log(`Reading stale data due to network issue : ${result.request?.stale}`)
-
-      return featFlagList as FeatureFlags
-    } catch (ex) {
-      console.error('Fallback to local feature flags', ex)
-      setFeatureFlagList(props.config.fallbackFlagValues)
-      return undefined
-    }
-  }
+  }, [featureFlagList, setFeatureFlagList, props])
 
   const getFeatureFlagByName = (name: string) => {
     if (!featureFlagList) return ''
